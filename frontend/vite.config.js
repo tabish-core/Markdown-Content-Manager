0335790ea4cc62d7e81a04dfad3d7b7cb1a23432
tabish-core <tabish.core@gmail.com>
2026-02-08 17:12:37 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "dist", // Ensures the build folder matches your Vercel settings
+    outDir: "dist",
   },
   server: {
     proxy: {
