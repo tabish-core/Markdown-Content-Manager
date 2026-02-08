9f97ad8f4f70db468e5f16e4412451ae75541a8d
tabish-core <tabish.core@gmail.com>
2026-02-08 17:09:18 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"; // Add this line
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      // This forces EVERYTHING to use the same React instance
-      react: path.resolve(__filename, "../node_modules/react"),
-      "react-dom": path.resolve(__filename, "../node_modules/react-dom"),
-    },
+  build: {
+    outDir: "dist", // Ensures the build folder matches your Vercel settings
   },
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:5000",
+        changeOrigin: true,
       },
     },
   },
