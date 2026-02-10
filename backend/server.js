f58718274bc03156f90b2103e8c0c32dcf0fe9cf
tabish-core <tabish.core@gmail.com>
2026-02-10 18:03:58 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -63,9 +63,9 @@ if (process.env.NODE_ENV === "production") {
 
 connectDB();
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server ready on port ${port}.`));
-
-module.exports = app; // <--- Vercel needs this!
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 export default app;
