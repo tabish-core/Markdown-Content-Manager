911d9f152eb923a171b3c3226eb7fa86e25887db
tabish-core <tabish.core@gmail.com>
2026-02-27 14:50:20 +0500

    back to MongoDB the GOAT :)

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,27 +1,11 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// export const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-//     console.log(`MongoDB connected: ${conn.connection.host}`);
-//   } catch (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
-// };
-
-import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-);
-
-export const connectDB = () => {
-  console.log("Supabase Client Initialized");
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
-
-//MONGO_URI=mongodb://127.0.0.1:27017/test
-//PORT=5000
